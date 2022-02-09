d97170ad88e5763fb94429061ab3274bdb412f35
Bernard Lee <bernard.hex@gmail.com>
2022-02-09 05:09:04 +0000

    content changes, removed gitpod.yaml

--- basics/demo/components/layout.js
+++ basics/demo/components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Shu Uesugi'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Bernard Lee'
+export const siteTitle = 'Blog'
 
 export default function Layout({ children, home }) {
   return (
