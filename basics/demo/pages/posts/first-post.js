b8ed3879f2e37e227368efd2c890581087da224e
Bernard Lee <bernard.hex@gmail.com>
2022-04-17 18:40:14 +0000

    commit

--- basics/demo/pages/posts/first-post.js
+++ basics/demo/pages/posts/first-post.js
@@ -10,6 +10,7 @@ export default function Experiments() {
       </Head>
       <article>
         <h1 className={utilStyles.headingXl}>Experiments</h1>
+        <iframe src="https://auto.kb.eastus2.azure.elastic-cloud.com:9243/app/r/s/icy-gigantic-cartoon" height="600" width="800"></iframe>
         <div className={utilStyles.lightText}>
             <h2>A test</h2>
             <p>The search box should be below</p>
@@ -18,4 +19,6 @@ export default function Experiments() {
     </Layout>
   )
 }
+
+
 
